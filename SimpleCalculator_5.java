94afbe9980356e585eb0ce63ffc6cf0ad931f82f
Gerald Bartlett <gerald.bartlett@hotmail.com>
2022-06-16 20:12:52 -0230

    Add files via upload

---
+++ SimpleCalculator_5.java
@@ -0,0 +1,51 @@
++// Lab Task 1.5
+
+import java.util.Scanner;
+
+class SimpleCalculator_5 {
+  public static void main(String[] args) {
+
+    char operator;
+    Double number1, number2, result;   
+    Scanner input = new Scanner(System.in);
+
+    //  enter operator
+    System.out.println("Choose an operator: addition: + " + "subtraction: - " + "Multiplication: * " + "division: /" );
+    operator = input.next().charAt(0);
+
+    //  enter numbers
+    System.out.println("Enter first number");
+    number1 = input.nextDouble();
+
+    System.out.println("Enter second number");
+    number2 = input.nextDouble();
+
+    switch (operator) {      
+      case '+':
+        result = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + result);
+        break;
+
+        case '-':
+        result = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + result);
+        break;
+
+      case '*':
+        result = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + result);
+        break;
+
+      case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;     
+
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    }
+
+    input.close();
+  }
+}
